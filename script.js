2664bfde83f3d6383dbed252a44dc2bad6cc073c
YashVarpe05 <yashvarpe2005@gmail.com>
2023-08-24 01:46:26 +0530

    mouse follower and heading hover effect added

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // Shery.mouseFollower();
 Shery.mouseFollower();
+Shery.makeMagent(".magnet");
+SHery.hoverWithMediaCircle(".hvr", {
+	videos: ["./assets/0.mp4", "./assets/2.mp4", "./assets/3.mp4"],
+});
