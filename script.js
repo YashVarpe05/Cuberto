7bc8b22d0d7fc509006b0eb154533b3d912f18e9
YashVarpe05 <yashvarpe2005@gmail.com>
2023-08-24 02:03:44 +0530

    add the title animation with scroll trigger

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Shery.mouseFollower();
 Shery.mouseFollower();
-Shery.makeMagent(".magnet");
-SHery.hoverWithMediaCircle(".hvr", {
-	videos: ["./assets/0.mp4", "./assets/2.mp4", "./assets/3.mp4"],
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", {
+	videos: ["./assets/0.mp40", "./assets/2.mp4", "./assets/3.mp4"],
 });
+
+gsap.to(".fleftelm", {
+	scrollTrigger: {
+		trigger: "#fimages",
+		pin: true,
+		start: "top top",
+		end: "bottom bottom",
+		endTrigger: ".last",
+		scrub: 1,
+	},
+	y: "-300%",
+	ease: Power1,
+});
+// shery.imageEffext(".image", {
+// 	style: 5,
+// 	slideStyle: (setScroll) => {
+// 		window.addEventListener("scroll", () => {
+// 			setScroll(window.scrollY / innerHeight);
+// 		});
+// 	},
+// });
