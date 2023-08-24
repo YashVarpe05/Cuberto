296d01f507b8107b320bbb1791f94c0449c07ef1
yash <yashvarpe2005@gmail.com>
2023-08-24 21:54:08 +0530

    registerPlugin of gsap

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Shery.makeMagnet(".magnet");
 Shery.hoverWithMediaCircle(".hvr", {
 	videos: ["./assets/0.mp4", "./assets/2.mp4", "./assets/3.mp4"],
 });
-
+gsap.registerPlugin(ScrollTrigger);
 gsap.to(".fleftlm", {
 	scrollTrigger: {
 		trigger: "#fimages",
@@ -21,7 +21,7 @@ gsap.to(".fleftlm", {
 let sections = document.querySelectorAll(".fleftlm");
 
 Shery.imageEffect(".images", {
-	style: 5,
+	style: 1,
 	config: { onMouse: { value: 1 } },
 	slideStyle: (setScroll) => {
 		sections.forEach(function (section, index) {
